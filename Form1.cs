b9b28dc949540aa97890f44aed05918ee971840d
Anna <jerofejeva.anna@yahoo.com>
2014-12-29 21:39:31 +0000

    Added button and text box, clicking button prints into the text box

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace ExploringWindowsFormApps
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("The button has been clicked\r\n");
+        }
     }
 }
